b5aee57de7167f914475d709e81c47f0d54f7bbe
luniemma <luniyisiemmanuel@gmail.com>
2024-09-15 14:42:09 -0400

    aks testing

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
-terraform {
-  backend "azurerm" {
-    # Note: The storage account name, container name, and key will be 
-    # provided via CLI arguments in the GitHub Actions workflow
-  }
-}
+# terraform {
+#   backend "azurerm" {
+#     # Note: The storage account name, container name, and key will be 
+#     # provided via CLI arguments in the GitHub Actions workflow
+#   }
+# }
--- main.tf
+++ main.tf
@@ -1,15 +1,15 @@
-# main.tf
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 3.0"
-    }
-  }
-}
+# # main.tf
+# terraform {
+#   required_providers {
+#     azurerm = {
+#       source  = "hashicorp/azurerm"
+#       version = "~> 3.0"
+#     }
+#   }
+# }
 provider "azurerm" {
   features  {}
-  skip_provider_registration = true
+  
 }
 
 module "resource_group" {
